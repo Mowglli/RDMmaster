fbd95a6e6f141c1e5d42a14d231e8878151d73e4
Jesper Egsgaard Nielsen <55810497+Mowglli@users.noreply.github.com>
2022-03-23 14:53:29 +0100

    Add files via upload

---
+++ RDMTEST_BIB.ino
@@ -0,0 +1,29 @@
+#include "RDMmaster.h"
+
+  RDMmasterClass RDM;
+  int DMX;
+  int fixturemode = 0;
+  int DMXAdress = 0;
+void setup(){
+
+  
+  RDM.initPins();
+  RDM.initRDMdata();
+  RDM.setDMXaddress(100);
+  delay(1000);
+  RDM.setFixtureMode(1);
+  delay(1000);
+  fixturemode = RDM.getFixtureMode();
+  Serial.print("FixtureMode: ");
+  Serial.println(fixturemode);
+  DMXAdress = RDM.getDMXAdress();
+  Serial.print("DMXAdress: ");
+  Serial.println(DMXAdress);
+
+  //RDM.DMXtransmit(1,20);
+  //RDM.DMXtransmit(2,50);
+  //RDM.DMXtransmit(11,255);
+}
+
+void loop() {
+}
